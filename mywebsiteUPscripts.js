83882190c9a4c2d841ddfc2a5295ac6d25edd539
owenccode <135147597+owenccode@users.noreply.github.com>
2023-09-14 11:11:56 -0500

    mywebsiteUPscripts.js

--- mywebsiteUPscripts.js
+++ mywebsiteUPscripts.js
@@ -166,3 +166,27 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('.toggletest').forEach(trigger => {
+  trigger.addEventListener('click', function(){ 
+    this.x = ((this.x || 0) + 1)%2; 
+    if(this.x){ 
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.remove('full'));
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.add('half'));
+      }
+      else{ 
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.remove('half'));
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.add('full'));
+      } 
+    });
+ });
+
+ document.querySelectorAll('').forEach(trigger => {
+  trigger.addEventListener('click', function(){ 
+ 
+    });
+ });
+});
+</script>
