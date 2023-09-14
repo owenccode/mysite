0d4a9b63f61c9b29520f14647da8f009ffd7a5ab
owenccode <135147597+owenccode@users.noreply.github.com>
2023-09-14 11:26:46 -0500

    toggletest.js

--- toggletest.js
+++ toggletest.js
@@ -1,1 +1,23 @@
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelectorAll('.toggletest').forEach(trigger => {
+  trigger.addEventListener('click', function(){ 
+    this.x = ((this.x || 0) + 1)%2; 
+    if(this.x){ 
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.remove('full'));
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.add('half'));
+      }
+      else{ 
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.remove('half'));
+      document.querySelectorAll('.toggletest').forEach(target => target.classList.add('full'));
+      } 
+    });
+ });
 
+ document.querySelectorAll('').forEach(trigger => {
+  trigger.addEventListener('click', function(){ 
+ 
+    });
+ });
+});
+</script>
