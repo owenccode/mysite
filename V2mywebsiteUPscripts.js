efc5ad3dd88119b1ad1be1509cc3041cd7841277
owenccode <135147597+owenccode@users.noreply.github.com>
2024-03-12 13:49:45 -0500

    Update V2mywebsiteUPscripts.js

--- V2mywebsiteUPscripts.js
+++ V2mywebsiteUPscripts.js
@@ -172,7 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
  document.querySelectorAll('.toggletest').forEach(trigger => {
   trigger.addEventListener('click', function(){ 
-    this.x = ((this.x || 0) + 1)%2; 
+    this.x = ((this.x || 1) + 1)%2; 
     if(this.x){ 
       document.querySelectorAll('.toggletest').forEach(target => target.classList.remove('full'));
       document.querySelectorAll('.toggletest').forEach(target => target.classList.add('half'));
